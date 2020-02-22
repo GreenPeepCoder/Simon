0f8491d795ddd7addc0d390776560edef6518ccf
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-22 15:18:08 -0500

    Implement #take_turn

--- lib/simon.rb
+++ lib/simon.rb
@@ -14,11 +14,14 @@ class Simon
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    round_success_message if @game_over == false
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
