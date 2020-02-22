ac3a93246152b58c66858da1b3bdcfd393d50f1b
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-22 15:28:29 -0500

    Fill in the rest of the program that the specs don't test for

--- lib/simon.rb
+++ lib/simon.rb
@@ -26,10 +26,24 @@ class Simon
 
   def show_sequence
     add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 0.75
+      system("clear")
+      sleep 0.25
+    end
   end
 
   def require_sequence
-
+    puts "Repeat the sequence by entering the first letter of each color on a new line."
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
   end
 
   def add_random_color
@@ -37,11 +51,11 @@ class Simon
   end
 
   def round_success_message
-
+    "Round successful. Here is the next sequence"
   end
 
   def game_over_message
-
+    "Game over. You made it #{sequence_length -1} rounds"
   end
 
   def reset_game
